ab8cdc701285b3f6259d80ae3515763e6bb19603
Jo@uww16 <jprbarbosa16@gmail.com>
2024-11-25 14:49:28 -0300

    Adição das legendas

--- frontend/totem/src/templates/poltrona/index.tsx
+++ frontend/totem/src/templates/poltrona/index.tsx
@@ -1,9 +1,8 @@
-import  { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { RiArmchairLine } from "react-icons/ri";
 import { TbArmchair2Off } from "react-icons/tb";
 import { InformationsContext } from "../../contexts/informationsProvider";
 import "./styles.scss";
-
 
 const Poltrona = () => {
   const [selectedSeats, setSelectedSeats] = useState<string[]>([]);
@@ -37,40 +36,65 @@ const Poltrona = () => {
     <div className="poltrona-page">
       <h1>CINEACH</h1>
       <h2>Selecione os assentos</h2>
-      <div className="container">
-        <div className="screen">TELA</div>
-        <div className="seats-container">
-          {seats.flat().map((seat) => (
-            <div
-              key={seat}
-              className={`seat ${
-                occupiedSeats.includes(seat)
-                  ? "occupied"
-                  : selectedSeats.includes(seat)
-                  ? "selected"
-                  : seat.startsWith("A")
-                  ? "vip"
-                  : "available"
-              }`}
-              onClick={() => toggleSeat(seat)}
-            >
-              {occupiedSeats.includes(seat) ? (
-                <TbArmchair2Off />
-              ) : (
-                <RiArmchairLine />
-              )}
-              <span>{seat}</span>
-            </div>
-          ))}
+      <div className="layout">
+        <div className="legend">
+          <p className="black">
+            <span className="legend-color"></span> Disponível
+          </p>
+          <p className="red">
+            <span className="legend-color"></span> Ocupado
+          </p>
+          <p className="yellow">
+            <span className="legend-color"></span> PCD
+          </p>
+        </div>
+  
+        {/* Container de poltronas */}
+        <div className="container">
+          <div className="screen">TELA</div>
+          <div className="seats-container">
+            {seats.flat().map((seat) => (
+              <div
+                key={seat}
+                className={`seat ${
+                  occupiedSeats.includes(seat)
+                    ? "occupied"
+                    : selectedSeats.includes(seat)
+                    ? "selected"
+                    : seat.startsWith("A")
+                    ? "vip"
+                    : "available"
+                }`}
+                onClick={() => toggleSeat(seat)}
+              >
+                {occupiedSeats.includes(seat) ? (
+                  <TbArmchair2Off />
+                ) : (
+                  <RiArmchairLine />
+                )}
+                <span>{seat}</span>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
       <div className="footer">
-        <button className="back-button" onClick={() => setTabActive(tabActive - 1)}>Voltar</button>
+        <button
+          className="back-button"
+          onClick={() => setTabActive(tabActive - 1)}
+        >
+          Voltar
+        </button>
         <span>{selectedSeats.length} assentos selecionados</span>
-        <button className="next-button" onClick ={() => setTabActive(tabActive + 1)}>Próximo</button>
+        <button
+          className="next-button"
+          onClick={() => setTabActive(tabActive + 1)}
+        >
+          Próximo
+        </button>
       </div>
     </div>
   );
-};
+}
 
 export default Poltrona;
